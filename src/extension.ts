b3ba1b3929a65460fb0dbc8867ab60c86bc9f274
Nick Sieger <nick@nicksieger.com>
2022-02-17 15:01:13 -0600

    Extract logic to tiltfile-client

--- src/extension.ts
+++ src/extension.ts
@@ -1,60 +1,14 @@
-import net = require('net');
-import { spawn } from 'child_process';
+import { ExtensionContext } from 'vscode';
 
-import { commands, window, workspace, ExtensionContext, OutputChannel, Disposable, } from 'vscode';
+import { TiltfileClient } from './tiltfile-client';
 
-import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-} from 'vscode-languageclient/node';
-
-let client: LanguageClient;
-let disposeClient: Disposable;
+let client: TiltfileClient;
 
 export function activate(context: ExtensionContext) {
-	const sharedArgs = ["start", "--builtin-paths=" + context.asAbsolutePath("data/api.py")]
-	const outputChannel = window.createOutputChannel('Tiltfile LSP');
-
-	registerCommands(context, outputChannel);
-
-	const serverOptions: ServerOptions = () => {
-		return isLspServerListening().then((listening: boolean) => new Promise((res) => {
-			if (listening) {
-				outputChannel.appendLine("Connect to existing server");
-				const socket = net.connect({host: "127.0.0.1", port: defaultLspPort});
-				res({writer: socket, reader: socket});
-			} else {
-				outputChannel.appendLine("Starting child process");
-				res(spawn("starlark-lsp", sharedArgs.concat(["--verbose", "--debug"])));
-			}
-		}))
-	};
-
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: [{ scheme: 'file', language: 'tiltfile' }],
-		synchronize: {
-			// Notify the server about file changes to relevant files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/Tiltfile')
-		},
-		outputChannel: outputChannel,
-		traceOutputChannel: outputChannel
-	};
-
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'tiltfile',
-		'Tiltfile LSP',
-		serverOptions,
-		clientOptions
-	);
-
-	// Start the client. This will also launch the server
-	disposeClient = client.start();
-
-	outputChannel.appendLine("Tiltfile LSP started");
-	outputChannel.show(true);
+	const client = new TiltfileClient(context);
+	client.start();
+	client.outputChannel.appendLine("Tiltfile LSP started");
+	client.outputChannel.show(true);
 }
 
 export function deactivate(): Thenable<void> | undefined {
@@ -64,45 +18,3 @@ export function deactivate(): Thenable<void> | undefined {
 	return client.stop();
 }
 
-const defaultLspPort = 8765;
-let wasListening = false;
-
-function isLspServerListening(): Promise<boolean> {
-	return new Promise((resolve) => {
-		const checkListen = () => {
-			var server = net.createServer(function(socket) {
-				socket.write("\r\n");
-				socket.pipe(socket);
-			});
-			server.on('error', function () {
-				wasListening = true;
-				resolve(true);
-			});
-			server.on('listening', function () {
-				server.close();
-				resolve(false);
-			});
-			server.listen(defaultLspPort, '127.0.0.1');
-		}
-
-		if (wasListening) {
-			setTimeout(checkListen, 2500);
-		} else {
-			checkListen();
-		}
-	});
-}
-
-function registerCommands(context: ExtensionContext, ch: OutputChannel) {
-	context.subscriptions.push(commands.registerCommand("tiltfile.restartServer", () => {
-		ch.appendLine("Restarting server 1");
-		client.stop().catch((e) => {
-			ch.appendLine("Error restarting: " + e);
-		}).then(() => {
-			disposeClient.dispose();
-			ch.appendLine("Restarting server 2");
-			wasListening = false;
-			disposeClient = client.start();
-		});
-	}));
-}
---
+++ src/tiltfile-client.ts
@@ -0,0 +1,84 @@
+import net = require('net');
+import { ChildProcess, spawn } from 'child_process';
+import { commands, ExtensionContext, window, workspace } from 'vscode';
+import { LanguageClient, LanguageClientOptions, StreamInfo } from 'vscode-languageclient/node';
+
+const debugLspPort  = 8760;
+const extensionLang = 'tiltfile';
+const extensionName = 'Tiltfile LSP';
+
+export class TiltfileClient extends LanguageClient {
+	private wasListening = false;
+
+	public constructor(private context: ExtensionContext) {
+		super(extensionLang, extensionName,
+			  () => this.serverOptions(),
+			  TiltfileClient.clientOptions());
+		this.registerCommands();
+	}
+
+	static clientOptions(): LanguageClientOptions {
+		const ch = window.createOutputChannel(extensionName);
+		return {
+			documentSelector: [{ scheme: 'file', language: extensionLang }],
+			synchronize: {
+				// Notify the server about file changes to relevant files contained in the workspace
+				fileEvents: workspace.createFileSystemWatcher('**/Tiltfile')
+			},
+			outputChannel: ch,
+			traceOutputChannel: ch
+		};
+	}
+
+	async serverOptions(): Promise<ChildProcess|StreamInfo> {
+		return this.isDebugLspServerListening().then((listening: boolean) => new Promise((res) => {
+			if (listening) {
+				this.outputChannel.appendLine("Connect to debug server");
+				const socket = net.connect({host: "127.0.0.1", port: debugLspPort});
+				res({writer: socket, reader: socket});
+			} else {
+				const sharedArgs = ["start", "--builtin-paths=" + this.context.asAbsolutePath("data/api.py")]
+				this.outputChannel.appendLine("Starting child process");
+				res(spawn("starlark-lsp", sharedArgs.concat(["--verbose", "--debug"])));
+			}
+		}));
+	}
+
+	isDebugLspServerListening(): Promise<boolean> {
+		return new Promise((resolve) => {
+			const checkListen = () => {
+				var server = net.createServer(function(socket) {
+					socket.write("\r\n");
+					socket.pipe(socket);
+				});
+				server.on('error', function () {
+					this.wasListening = true;
+					resolve(true);
+				});
+				server.on('listening', function () {
+					server.close();
+					resolve(false);
+				});
+				server.listen(debugLspPort, '127.0.0.1');
+			}
+
+			if (this.wasListening) { // wait for server to restart
+				setTimeout(checkListen, 2500);
+			} else {
+				checkListen();
+			}
+		});
+	}
+
+	public registerCommands() {
+		this.context.subscriptions.push(commands.registerCommand("tiltfile.restartServer", () => {
+			this.outputChannel.appendLine("Restarting server 1");
+			this.stop().catch((e) => {
+				this.outputChannel.appendLine("Error restarting: " + e);
+			}).then(() => {
+				this.outputChannel.appendLine("Restarting server 2");
+				this.start();
+			});
+		}));
+	}
+}
